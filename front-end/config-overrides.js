2dd260c35f9d3786c28fb22148597c92b6c568bd
siyuan.zhu <zhusiyuanhao@163.com>
2019-11-05 00:07:48 +0800

    9888

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -1,21 +1,41 @@
-const chalk = require("chalk");
-const path = require("path");
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+  addWebpackAlias, addWebpackPlugin, setWebpackPublicPath, adjustWorkbox
+} = require("customize-cra");
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const chalk = require('chalk');
+const path = require('path');
 const { appBuild, publicPath, plugins } = require("./config");
-console.log(chalk.blue(`your buildPath is ${appBuild}\n`));
+
 function resolve(dir) {
-  return path.join(__dirname, ".", dir);
+  return path.join(__dirname, '.', dir)
 }
+
 module.exports = {
-  webpack: function(config, env) {
-    config.output.publicPath = publicPath;
-    config.resolve.alias = { ...config.resolve.alias, "@": resolve("src") };
-    plugins.forEach(p => {
-      config.plugins.push(p);
-    });
-    return config;
-  },
-  paths: function(paths, env) {
+  webpack: override(
+    fixBabelImports("import", {
+      libraryName: "antd",
+      libraryDirectory: "es",
+      style: "css" // change importing css to less
+    }),
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: { "@primary-color": "#1DA57A" }
+    }),
+    addWebpackAlias({
+      "@": resolve("src")
+    }),
+    addWebpackPlugin(new ProgressBarPlugin({
+      complete: "█",
+      format: `${chalk.green('Building')} [ ${chalk.green(':bar')} ] ':msg:' ${chalk.bold('(:percent)')}`,
+      clear: true
+    })
+    ), setWebpackPublicPath(publicPath)),
+  paths: (paths) => {
     paths.appBuild = appBuild;
+
     return paths;
   }
-};
+}
--- front-end/config.js
+++ front-end/config.js
@@ -1,16 +1,4 @@
-const chalk = require("chalk");
-const ProgressBarPlugin = require("progress-bar-webpack-plugin");
-
 module.exports = {
   publicPath: "./",
-  appBuild: `/Users/siyuan.zhu/Documents/zhuzhu-work/react-tpl/react-tpl/front-end/build`,
-  plugins: [
-    new ProgressBarPlugin({
-      complete: "█",
-      format: `${chalk.green("Building")} [ ${chalk.green(
-        ":bar"
-      )} ] ':msg:' ${chalk.bold("(:percent)")}`,
-      clear: true
-    })
-  ]
-};
+  appBuild: `/home/ppx/文档/tmp/build `
+}
