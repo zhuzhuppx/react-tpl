2ba0dab56df6fdef80c20119c222e41eda96668c
siyuan.zhu <zhusiyuanhao@163.com>
2019-11-05 22:14:39 +0800

    9999

--- front-end/src/pages/login/index.js
+++ front-end/src/pages/login/index.js
@@ -3,80 +3,86 @@ import "./index.less";
 import { Form, Icon, Input, Button, Checkbox } from "antd";
 import bgImg from "@/assets/timg.jpeg";
 class LoginPage extends React.Component {
-  componentDidMount() {
-    document.body.style.overflow = "hidden";
-  }
-  componentWillUnmount() {
-    document.body.style.overflow = "auto";
-  }
-  handleSubmit = e => {      
-    const history = this.props.history;
-   
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log("Received values of form: ", values);
-        history.push("/main");
-      }
-    });
-  };
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div className="login-page">
-        <div className="bg-img">
-          <img src={bgImg}></img>
-        </div>
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator("username", {
-              rules: [
-                { required: true, message: "Please input your username!" }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="Username"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("password", {
-              rules: [
-                { required: true, message: "Please input your Password!" }
-              ]
-            })(
-              <Input
-                prefix={
-                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                type="password"
-                placeholder="Password"
-              />
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator("remember", {
-              valuePropName: "checked",
-              initialValue: true
-            })(<Checkbox>Remember me</Checkbox>)}
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
-              Log in
-            </Button>
-            Or <a href="">register now!</a>
-          </Form.Item>
-        </Form>
-      </div>
-    );
-  }
+	componentDidMount() {
+		document.body.style.overflow = "hidden";
+	}
+	componentWillUnmount() {
+		document.body.style.overflow = "auto";
+	}
+	handleSubmit = e => {
+		const history = this.props.history;
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			if (!err) {
+				console.log("Received values of form: ", values);
+				history.push("/main");
+			}
+		});
+	};
+	render() {
+		const { getFieldDecorator } = this.props.form;
+		return (
+			<div className="login-page">
+				<div className="bg-img">
+					<img src={bgImg}></img>
+				</div>
+				<Form onSubmit={this.handleSubmit} className="login-form">
+					<Form.Item>
+						{getFieldDecorator("username", {
+							rules: [
+								{
+									required: true,
+									message: "请输入用户名"
+								}
+							]
+						})(
+							<Input
+								prefix={
+									<Icon
+										type="user"
+										style={{ color: "rgba(0,0,0,.25)" }}
+									/>
+								}
+								placeholder="用户名"
+							/>
+						)}
+					</Form.Item>
+					<Form.Item>
+						{getFieldDecorator("password", {
+							rules: [
+								{
+									required: true,
+									message: "请输入密码"
+								}
+							]
+						})(
+							<Input
+								prefix={
+									<Icon
+										type="lock"
+										style={{ color: "rgba(0,0,0,.25)" }}
+									/>
+								}
+								type="password"
+								placeholder="密码"
+							/>
+						)}
+					</Form.Item>
+					<Form.Item>
+				
+						
+						<Button
+							type="primary"
+							htmlType="submit"
+							className="login-form-button"
+						>
+						 登录
+						</Button>
+				
+					</Form.Item>
+				</Form>
+			</div>
+		);
+	}
 }
 export default Form.create({ name: "form_login" })(LoginPage);
