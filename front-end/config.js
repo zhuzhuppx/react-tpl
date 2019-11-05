230e7ed07125ed04974bee4556ea2bb2ca55ad2f
siyuan.zhu <siyuan.zhu@zhaopin.com.cn>
2019-11-05 17:26:49 +0800

    999

--- front-end/config.js
+++ front-end/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  proxyUrl: "http://localhost:9870",
   publicPath: "./",
   appBuild: `/home/ppx/文档/tmp/build `
 };
--- front-end/src/setupProxy.js
+++ front-end/src/setupProxy.js
@@ -1,9 +1,9 @@
 const proxy = require("http-proxy-middleware");
 // proxy 中间件的选择项
 
-let serverUrl = require('../proxyUrl');
+let {proxyUrl} = require('../config');
 var options = {
-	target: serverUrl,
+	target: proxyUrl,
 	changeOrigin: true,
 	pathRewrite: {
 		"^/api": "/"
