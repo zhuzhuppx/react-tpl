4fddd110537d9f77c043c917edc429e9646f0ed9
siyuan.zhu <siyuan.zhu@zhaopin.com.cn>
2019-11-06 18:41:07 +0800

    000

--- front-end/src/util/userUtil.js
+++ front-end/src/util/userUtil.js
@@ -1,8 +1,7 @@
-const key = 'react-user-key'
-export const setUser=(user)=>{
-   
-    localStorage.setItem(key,JSON.stringify(user))
-}
-export const getUser=()=>{
-    return JSON.parse(localStorage.getItem(key))
-}
+const key = "react-user-key";
+export const setUser = user => {
+  localStorage.setItem(key, JSON.stringify(user));
+};
+export const getUser = () => {
+  return JSON.parse(localStorage.getItem(key));
+};
